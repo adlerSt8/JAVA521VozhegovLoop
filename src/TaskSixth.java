6ff9cb5dad2684b13e6008047050a80ab197212a
Андрей <adlerSt@yandex.ru>
2025-02-15 12:38:34 +0300

    final

--- src/TaskSixth.java
+++ src/TaskSixth.java
@@ -4,9 +4,7 @@ public class TaskSixth {
     public static void main(String[] args) {
         System.out.println("Все нечетные числа в диапазоне от 1 до 20: ");
         for (int i = 1; i < 20; i++) {
-            if (i % 2 == 0) {
-                continue;
-            } else {
+            if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
         }
